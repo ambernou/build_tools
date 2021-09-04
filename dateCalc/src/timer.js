f321085de31b17fc12328d57600f7b5c3b0d7fd6
NatalyaSemenova <ambernou@gmail.com>
2021-09-05 01:53:56 +0700

    timer without stop
    timer without stop

--- dateCalc/src/timer.js
+++ dateCalc/src/timer.js
@@ -1,6 +1,7 @@
 //import { DateTime } from "./luxon.js";
 //import { counter } from "./timer_counter.js";
 import { formatError } from "./utils.js";
+//import { Howl, Howler } from "../../../howler.js-master/dist/howler.js";
 
 export const timerFormEl = document.getElementById("timer");
 export const timerCounterEl = document.getElementById("timer_counter");
@@ -11,6 +12,7 @@ timerFormEl.addEventListener("submit", handleStartTimer);
 
 function handleStartTimer(event) {
     event.preventDefault();
+    startBtnEl.setAttribute('disabled', true);
     timerCounterEl.innerHTML = "";
 
     let { hours, minutes, seconds } = event.target.elements;
@@ -25,10 +27,9 @@ function handleStartTimer(event) {
     </span>
     `;
     
-    //console.log(DateTime.fromISO());
-    
     if (seconds == 0) {
         timerCounterEl.innerHTML = formatError("Для запуска таймера надо заполнить хотя бы секунды");
+        startBtnEl.removeAttribute('disabled');
     }
     //counter(hours, minutes, seconds);
 
@@ -40,10 +41,30 @@ function handleStartTimer(event) {
                 ${hours} : ${minutes} : ${seconds}
             </span>
             `;
-            if (seconds == 0)
-                console.log("end!");
-        }
-    }
+        } else if (minutes > 0) {
+            minutes--;
+            seconds = 59;
             
+            timerCounterEl.innerHTML = `
+            <span> 
+                ${hours} : ${minutes} : ${seconds}
+            </span>
+            `;
+        } else if (hours > 0) {
+            hours--;
+            minutes = 59;
+            
+            timerCounterEl.innerHTML = `
+            <span> 
+                ${hours} : ${minutes} : ${seconds}
+            </span>
+            `;
+        }
+        // } else if (seconds == 0 && minutes == 0) {
+        //      console.log("end!");
+        // }
+        
+    }
+
     setInterval(counter, 1000);
 }
